7be72e1cde80397b4d03be72ec973dea2518e5c7
tamakyi <akakari@vip.qq.com>
2023-12-30 23:25:49 +0800

    Update registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://tamakyi.github.io/statusv2/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://tamakyi.github.io/statusv2/sw.js', { scope: '/statusv2/' })})}
